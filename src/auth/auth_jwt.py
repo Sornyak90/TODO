f3852cc7bb5cfc0ec57f01fda13eff44e0ead47f
Your Name <you@example.com>
2025-11-19 06:55:03 +0500

    feat auth2

---
+++ src/auth/auth_jwt.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from typing import Annotated
+from auth.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from jose import JWTError, jwt
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+def create_access_token(data: dict, expires_delta: timedelta | None = None):    
+    to_encode = data.copy()    
+    if expires_delta:        
+        expire = datetime.utcnow() + expires_delta    
+    else:        
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)    
+    to_encode.update({"exp": expire})    
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)    
+    return encoded_jwt
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):    
+    try:        
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("username")        
+        if username is None:
+            raise HTTPException(status_code=401, detail="Could not validate credentials")        
+        user = User(username=username) 
+
+        if user is None:            
+            raise HTTPException(status_code=401, detail="User not found")        
+        return user    
+    except JWTError:        
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
---
+++ src/auth/fake_db.py
@@ -0,0 +1,10 @@
+fake_users = {
+    "admin": {  # username как ключ
+        "username": "admin",
+        "password": "admin",
+    },
+    "johndoe": {
+        "username": "johndoe", 
+        "password": "password123",
+    }
+}
